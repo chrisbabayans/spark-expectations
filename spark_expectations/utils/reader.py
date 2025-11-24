cf0e9be0722f09c0f016f61b4e645b89961cf751
Surendra Athota <surendra004@gmail.com>
2025-11-24 15:07:45 -0500

    set_min_conf_sl

--- spark_expectations/utils/reader.py
+++ spark_expectations/utils/reader.py
@@ -44,7 +44,16 @@ class SparkExpectationsReader:
                 _default_notification_dict = {
                     "spark.expectations.notifications.email.enabled": False,
                     "spark.expectations.notifications.slack.enabled": False,
-                    "spark.expectations.notifications.teams.enabled": False
+                    "spark.expectations.notifications.teams.enabled": False,
+                    "spark.expectations.agg.dq.detailed.stats": False,
+                    "spark.expectations.query.dq.detailed.stats": False,
+                    "spark.expectations.notifications.on.start": False,
+                    "spark.expectations.notifications.on.completion": True,
+                    "spark.expectations.notifications.on.fail": True,
+                    "park.expectations.notifications.on.error.drop.exceeds.threshold.breach": False,
+                    "spark.expectations.notifications.on.rules.action.if.failed.set.ignore": False,
+                    "spark.expectations.job.metadata": "",
+                    "spark.expectations.notifications.slack.min.priority": "medium"
                 }
 
             _notification_dict: Dict[str, Union[str, int, bool, Dict[str, str]]] = (
